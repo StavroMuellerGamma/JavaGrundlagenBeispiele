5f4bb0beb7c1eaf159bb61f708010d190cb3d765
StavroMuellerGamma <a83nwa@gmail.com>
2016-12-21 17:10:20 +0100

    logische Ausdrücke mit atomaren Datentypen

--- Beispiele/src/LogischeAusdrücke.java
+++ Beispiele/src/LogischeAusdrücke.java
@@ -4,6 +4,63 @@
 public class LogischeAusdrücke {
     public static void main(String... args) {
         booleanBeispiele();
+        atomareDatentypenBeispiele();
+    }
+
+    private static void atomareDatentypenBeispiele() {
+        int i = 0, j = 1;
+        char c = 'a', d = 'b';
+        boolean a;
+        // int
+        // Und
+        a = (i == 0) && (j == 0);
+        System.out.println("(i == 0) && (j == 0) ist " + a);
+        // Oder
+        a = (i == 0) || (j == 0);
+        System.out.println("(i == 0) || (j == 0) ist " + a);
+        // Gleichheit
+        a = i == j;
+        System.out.println("i == j ist " + a);
+        // Ungleichheit
+        a = i != j;
+        System.out.println("i != j ist " + a);
+        // Größer
+        a = i > j;
+        System.out.println("i > j ist " + a);
+        // Kleiner
+        a = i < j;
+        System.out.println("i < j ist " + a);
+        // Größer-Gleich
+        a = i >= j;
+        System.out.println("i >= j ist " + a);
+        // Kleiner-Gleich
+        a = i <= j;
+        System.out.println("i <= j ist " + a);
+        // char
+        // Und
+        a = (c == 'a') && (d == 'a');
+        System.out.println("(c == 'a') && (d == 'a') ist " + a);
+        // Oder
+        a = (c == 'a') || (d == 'a');
+        System.out.println("(c == 'a') || (d == 'a') ist " + a);
+        // Gleichheit
+        a = c == d;
+        System.out.println("c == d ist " + a);
+        // Ungleichheit
+        a = c != d;
+        System.out.println("c != d ist " + a);
+        // Größer
+        a = c > d;
+        System.out.println("c > d ist " + a);
+        // Kleiner
+        a = c < d;
+        System.out.println("c < d ist " + a);
+        // Größer-Gleich
+        a = c >= d;
+        System.out.println("c >= d ist " + a);
+        // Kleiner-Gleich
+        a = c <= d;
+        System.out.println("c <= d ist " + a);
     }
 
     /**
